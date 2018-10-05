f46f2db691ab6a7ff3debaadd8c806bce9efef30
Fedor Arbuzov <arbuzovfp@gmail.com>
2018-10-05 16:50:20 +0300

    fix

--- tests/test_hangman.py
+++ tests/test_hangman.py
@@ -1,2 +1,6 @@
-def test_greet_the_world():
-    assert 1 == 1
+from hangman.hangman import get_random_string, game_process
+
+
+def test_get_random_string():
+    for i in range(100):
+        assert True == get_random_string() in ['def', 'class', 'while', 'for']

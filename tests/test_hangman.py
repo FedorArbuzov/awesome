5f6a553552e50852dcaf0f7cee099941eadecdc9
Fedor Arbuzov <arbuzovfp@gmail.com>
2018-10-05 16:57:00 +0300

    fix

--- tests/test_hangman.py
+++ tests/test_hangman.py
@@ -3,4 +3,4 @@ from hangman.hangman import get_random_string, game_process
 
 def test_get_random_string():
     for i in range(100):
-        assert True == get_random_string() in ['def', 'class', 'while', 'for']
+        assert True == (get_random_string() in ['def', 'class', 'while', 'for'])

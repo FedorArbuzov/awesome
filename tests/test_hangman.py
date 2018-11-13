19061ef31a1d818bc2795201e437baf94de91e5c
Fedor <fedor@localhost.ru>
2018-11-13 19:24:09 +0300

    new project

--- tests/test_hangman.py
+++
@@ -1,6 +0,0 @@
-from hangman.hangman import get_random_string, game_process
-
-
-def test_get_random_string():
-    for i in range(100):
-        assert True == (get_random_string() in ['def', 'class', 'while', 'for'])
